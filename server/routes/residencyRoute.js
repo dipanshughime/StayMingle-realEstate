90731d93f53fbdbb75f203e636d1dc953b52e2c0
Dipanshu ghime <dipanshughime@gmail.com>
2023-08-09 10:15:56 +0530

    residency controller done

--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,7 +1,9 @@
 import express, { Router } from 'express'; // Correct the import of Router
-import { createResidency } from '../controllers/residencyController.js';
+import { createResidency, getAllResidencies, getResidency } from '../controllers/residencyController.js';
 
 const router = Router(); // Use Router() with a capital "R"
 
 router.post ("/create",createResidency)
+router.get("/allresd", getAllResidencies)
+router.get("/:id", getResidency)
 export { router as residencyRoute };  
