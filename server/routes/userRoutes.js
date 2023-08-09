c607e821bc788c6381b3efe99803fcf20853fc03
Dipanshu ghime <dipanshughime@gmail.com>
2023-08-09 11:34:55 +0530

    cancel booking , get all book visites and book visites created

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express, { Router } from 'express'; // Correct the import of Router
-import { createUser } from '../controllers/userController.js';
+import { bookVisit, cancelBooking, createUser, getAllBookings } from '../controllers/userController.js';
 
 const router = Router(); // Use Router() with a capital "R"
 
 router.post("/register", createUser);
-
+router.post("/bookVisit/:id", bookVisit);
+router.post("/allBookings",getAllBookings);
+router.post("/removeBooking/:id",cancelBooking);
 export { router as userRoute };
